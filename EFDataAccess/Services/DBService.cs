4177d1f70bc1396452d85701c1b46785a8ea713b
llNovall <25434267+llNovall@users.noreply.github.com>
2023-04-24 16:49:12 +0200

    Added DBService to EFDataccess

---
+++ EFDataAccess/Services/DBService.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Domain.Services;
+using EFDataAccess.Contexts;
+using EFDataAccess.Repositories;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccess.Services
+{
+    public class DBService : IDBService
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly ICommentRepository _commentRepository;
+        private readonly ITagRepository _tagRepository;
+
+        public DBService(ApplicationDbContext context)
+        {
+            _postRepository = new PostRepository(context);
+            _commentRepository = new CommentRepository(context);
+            _tagRepository = new TagRepository(context);
+        }
+
+        public IPostRepository PostRepository
+        { get { return _postRepository; } }
+
+        public ICommentRepository CommentRepository
+        { get { return _commentRepository; } }
+
+        public ITagRepository TagRepository
+        { get { return _tagRepository; } }
+    }
+}
