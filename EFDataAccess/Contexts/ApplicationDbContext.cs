ed5477cd752c0d649aabfb98de9d0322fb1c89cf
llNovall <25434267+llNovall@users.noreply.github.com>
2023-04-24 16:48:34 +0200

    Added DbContext

---
+++ EFDataAccess/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace EFDataAccess.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
