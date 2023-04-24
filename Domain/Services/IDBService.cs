64b54119da10fcd4c9a636cc7bee9729e9bcff75
llNovall <25434267+llNovall@users.noreply.github.com>
2023-04-24 16:48:00 +0200

    Added interface DBService to Domain

---
+++ Domain/Services/IDBService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public interface IDBService
+    {
+        IPostRepository PostRepository { get; }
+        ICommentRepository CommentRepository { get; }
+        ITagRepository TagRepository { get; }
+    }
+}
