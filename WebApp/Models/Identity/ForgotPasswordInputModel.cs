4e6a877526b93aaca3025c872cd6c9a1b7659f17
llNovall <25434267+llNovall@users.noreply.github.com>
2023-04-26 19:36:15 +0200

    Added ForgotPasswordInput Model

---
+++ WebApp/Models/Identity/ForgotPasswordInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models.Identity
+{
+    public class ForgotPasswordInputModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+    }
+}
