6696ab22ad63404dfb7f19a4e5799ea9d43fa5b8
llNovall <25434267+llNovall@users.noreply.github.com>
2023-04-26 19:36:37 +0200

    Added Login page

---
+++ WebApp/Models/Identity/LoginInputModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebApp.Models.Identity
+{
+    public class LoginInputModel
+    {
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        public string Password { get; set; } = null!;
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
