a6a9674ab686bcd9f1efe5e4e813988cce9d031a
llNovall <25434267+llNovall@users.noreply.github.com>
2023-04-26 19:37:04 +0200

    Added controller for logout

---
+++ WebApp/Controllers/Account/LogoutController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers.Account
+{
+    public class LogoutController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<LogoutController> _logger;
+
+        public LogoutController(SignInManager<IdentityUser> signInManager, ILogger<LogoutController> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
