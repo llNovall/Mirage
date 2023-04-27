793105eb5f55c22d067b9ef37d475d856994c2d0
llNovall <25434267+llNovall@users.noreply.github.com>
2023-04-27 17:15:26 +0200

    Renamed Post to Blog for clarity

---
+++ WebApp/Controllers/Blog/BlogController.cs
@@ -0,0 +1,76 @@
+﻿using Domain.Entities;
+using Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models.Blog;
+
+namespace WebApp.Controllers.Blog
+{
+    public class BlogController : Controller
+    {
+        private readonly IDBService _db;
+        private readonly ILogger<BlogController> _logger;
+        private readonly EventId _eventId;
+
+        public BlogController(IDBService dBService, ILogger<BlogController> logger)
+        {
+            _db = dBService;
+            _logger = logger;
+            _eventId = new EventId(200, name: "PostController");
+        }
+
+        public async Task<IActionResult> Index(string blogPostId)
+        {
+            BlogPost? blogPost = await _db.PostRepository.FindByIdIncludeNavigationAsync(blogPostId);
+
+            return View(blogPost);
+        }
+
+        public IActionResult Create()
+        {
+            _logger.LogDebug(_eventId, "Displaying view to create post.");
+            BlogCreateModel model = new();
+            List<Tag> tags = _db.TagRepository.GetAll().ToList();
+
+            foreach (Tag tag in tags)
+            {
+                model.TagsList.Add(new TagCheckItem { TagId = tag.Id.ToString(), TagName = tag.TagName, IsChecked = false });
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAsync(BlogCreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                List<TagCheckItem> checkedTags = model.TagsList.Where(tag => tag.IsChecked).ToList();
+                List<Tag> tags = _db.TagRepository.GetAll().ToList();
+
+                List<Guid> tagIds = new();
+
+                checkedTags.Select(c => c.TagId).ToList().ForEach(c => tagIds.Add(Guid.Parse(c)));
+
+                List<Tag> selectedTags = tags.Where(c => tagIds.Contains(c.Id)).ToList();
+
+                BlogPost blogPost = new()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = model.Title,
+                    AuthorId = Guid.NewGuid(),
+                    BodyContent = model.BodyContent,
+                    Tags = selectedTags,
+                };
+
+                int result = await _db.PostRepository.AddAsync(blogPost);
+
+                if (result > 0)
+                {
+                    return RedirectToAction("Index", new { blogPostId = blogPost.Id.ToString() });
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ WebApp/Models/Blog/BlogCreateModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models.Blog
+{
+    public class BlogCreateModel
+    {
+        [StringLength(maximumLength: 100, MinimumLength = 10)]
+        public string Title { get; set; } = null!;
+
+        [Display(Name = "Content")]
+        [StringLength(maximumLength: 4000, MinimumLength = 100)]
+        public string BodyContent { get; set; } = null!;
+
+        [Display(Name = "Select Tag / Tags from below")]
+        public List<TagCheckItem> TagsList { get; set; } = new();
+    }
+
+    public class TagCheckItem
+    {
+        public string TagId { get; set; } = null!;
+        public string TagName { get; set; } = null!;
+        public bool IsChecked { get; set; }
+    }
+}
