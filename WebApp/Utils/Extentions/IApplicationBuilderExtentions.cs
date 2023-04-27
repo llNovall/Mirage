40b68cb8f46dfbebef2633a0e7f4d6e7f2650531
llNovall <25434267+llNovall@users.noreply.github.com>
2023-04-27 17:23:37 +0200

    Added an extention to IApplicationBuilder which adds functionality to check if db is created and seed test data

---
+++ WebApp/Utils/Extentions/IApplicationBuilderExtentions.cs
@@ -0,0 +1,36 @@
+﻿using EFDataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Utils.Extentions
+{
+    public static class IApplicationBuilderExtentions
+    {
+        /// <summary>
+        /// Creates DbContext if required
+        /// </summary>
+        /// <param name="builder"></param>
+        public static void EnsureIdentityDbCreated(this IApplicationBuilder builder)
+        {
+            using (var serviceScope = builder.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                ApplicationDbContext dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                dbContext.Database.Migrate();
+            }
+        }
+
+        /// <summary>
+        /// Seeds database with test data. Use this only for development.
+        /// </summary>
+        /// <param name="builder"></param>
+        public static void SeedTestDataToDatabase(this IApplicationBuilder builder)
+        {
+            using (var serviceScope = builder.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                ApplicationDbContext dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                dbContext.SeedTestData();
+            }
+        }
+    }
+}
