8c34b449e4405151b3bfad9d2d7279fd780ab091
llNovall <25434267+llNovall@users.noreply.github.com>
2023-05-15 16:00:27 +0200

    Added support for azure storage

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -7,7 +7,9 @@ using System.Net;
 using WebApp.Utils.Extentions;
 using WebApp.Authorization;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
+using WebApp.Services;
+using Microsoft.Extensions.Azure;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,22 +25,49 @@ if (builder.Environment.IsDevelopment())
 else
 {
     builder.Services.AddDbContext<ApplicationDbContext>(
-    options =>
-    {
-        options.UseSqlServer(connectionString: builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING"));
-    }
-);
+        options =>
+        {
+            options.UseSqlServer(connectionString: builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING"));
+        }
+    );
+}
+
+if (builder.Environment.IsDevelopment())
+{
+    ClientSecretCredential clientSecretCredential = new(tenantId: builder.Configuration["azure-storage:AZURE_TENANT_ID"],
+    clientId: builder.Configuration["azure-storage:AZURE_CLIENT_ID"],
+    clientSecret: builder.Configuration["azure-storage:AZURE_CLIENT_SECRET"]);
+
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+    string azureStorageUrl = builder.Configuration["azure-storage:STORAGE_URL"];
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+    builder.Services.AddAzureClients(c =>
+        {
+#pragma warning disable CS8604 // Possible null reference argument.
+            c.AddBlobServiceClient(new Uri(azureStorageUrl));
+#pragma warning restore CS8604 // Possible null reference argument.
+
+            c.UseCredential(clientSecretCredential);
+        }
+    );
+}
+else
+{
+    builder.Services.AddAzureClients(c =>
+        c.AddBlobServiceClient(connectionString: builder.Configuration.GetConnectionString("AZURE_STORAGEBLOB_CONNECTIONSTRING"))
+    );
 }
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
-    options.Stores.ProtectPersonalData = false;
     options.User.RequireUniqueEmail = true;
     options.SignIn.RequireConfirmedAccount = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequiredLength = 6;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireLowercase = false;
+    options.Password.RequireDigit = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireUppercase = true;
 }
 ).AddEntityFrameworkStores<ApplicationDbContext>();
 
@@ -49,6 +78,7 @@ builder.Services.AddHttpsRedirection(options =>
 });
 
 builder.Services.AddMvc();
+
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("CommentEdit",
@@ -78,12 +108,15 @@ builder.Services.AddScoped<IDBService, DBService>();
 builder.Services.AddScoped<IAuthorizationHandler, SameAuthorIDHandler>();
 builder.Services.AddScoped<IAuthorizationHandler, SameAuthorCommentHandler>();
 builder.Services.AddScoped<IAuthorizationHandler, SameAuthorBlogPostHandler>();
+builder.Services.AddScoped<IAzureStorageService, AzureStorageService>();
+
 builder.Services.ConfigureApplicationCookie(options =>
 {
     // Cookie settings
     options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-
+    options.Cookie.MaxAge = TimeSpan.FromDays(1);
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    options.Cookie.Expiration = TimeSpan.FromMinutes(60);
     options.LoginPath = "/Identity/Account/Login";
     options.AccessDeniedPath = "/Identity/Account/AccessDenied";
     options.SlidingExpiration = true;
@@ -116,6 +149,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
---
+++ WebApp/Services/AzureStorageService.cs
@@ -0,0 +1,76 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Domain.Services;
+using Azure.Storage;
+using Microsoft.Extensions.Logging;
+using Azure.Storage.Blobs.Models;
+
+namespace WebApp.Services
+{
+    public class AzureStorageService : IAzureStorageService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly ILogger<AzureStorageService> _logger;
+        private readonly string _containerName;
+        private readonly EventId _eventId;
+
+        public AzureStorageService(BlobServiceClient blobServiceClient, ILogger<AzureStorageService> logger)
+        {
+            _blobServiceClient = blobServiceClient;
+            _logger = logger;
+            _containerName = "blog";
+            _eventId = new EventId(301, name: "AzureStorageService");
+        }
+
+        /// <summary>
+        /// Generates a file name in format : <paramref name="containerName"/>-Guid-Date
+        /// </summary>
+        /// <param name="containerName"></param>
+        /// <returns></returns>
+        public static string GenerateFileName(string containerName) => $"{containerName}-{Guid.NewGuid()}-{DateTime.UtcNow:dd-M-yy}";
+
+        public async Task<string> UploadImageAsync(string imgType, Stream imgStream)
+        {
+            try
+            {
+                _logger.LogInformation(_eventId, "Attempting to upload image.");
+                BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+
+                string imgId = GenerateFileName(_containerName);
+
+                var response = await blobContainerClient.UploadBlobAsync(imgId, imgStream, CancellationToken.None);
+
+                if (response.GetRawResponse().Status == 201)
+                {
+                    _logger.LogInformation(_eventId, "Image uploaded successfully.");
+
+                    var blobImg = blobContainerClient.GetBlobClient(imgId);
+                    await blobImg.SetHttpHeadersAsync(new BlobHttpHeaders { ContentType = imgType });
+
+                    return blobImg.Uri.AbsoluteUri;
+                }
+            }
+            catch (RequestFailedException ex)
+            {
+                _logger.LogCritical(_eventId, "AZURE STORAGE - REQUEST FAILED", ex);
+            }
+
+            return "";
+        }
+
+        //public async Task UploadImageFromStreamAsync()
+        //{
+        //    BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+
+        //    blobContainerClient.UploadBlobAsync()
+        //}
+
+        //public async Task<BlobContainerClient> CreateContainerAsync(string containerName)
+        //{
+        //    try
+        //    {
+        //        BlobContainerClient container = await _blobServiceClient.GetBlobContainerClient()
+        //    }
+        //}
+    }
+}
