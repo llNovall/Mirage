02e05c2251d1431674d392ce812e72dcf248ccf2
llNovall <25434267+llNovall@users.noreply.github.com>
2023-05-16 16:40:53 +0200

    Added view for contact

---
+++ WebApp/Controllers/ContactController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models.Contact;
+
+namespace WebApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IEmailSender _emailSender;
+
+        public ContactController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        [RequireHttps]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [RequireHttps]
+        [AllowAnonymous]
+        public async Task<IActionResult> Send(ContactInputModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _emailSender.SendEmailAsync(model.EmailAddress, model.Subject, model.Message);
+                }
+                catch (Exception ex)
+                {
+                }
+
+                return View(model);
+            }
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ WebApp/Models/Contact/ContactInputModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models.Contact
+{
+    public class ContactInputModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        [MinLength(1)]
+        [DataType(DataType.Text)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [DataType(dataType: DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string EmailAddress { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Subject")]
+        [MinLength(5)]
+        [DataType(DataType.Text)]
+        public string Subject { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Message")]
+        [MinLength(5)]
+        [DataType(DataType.Text)]
+        public string Message { get; set; } = null !;
+    }
+}
